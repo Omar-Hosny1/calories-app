2e6d3263029a4ffd007c61ebde65a739e8b6c206
Omar <om300019@gmail.com>
2022-09-15 13:12:45 +0200

    Cahnge scrolling

--- src/components/Result/Disches/Dishes.js
+++ src/components/Result/Disches/Dishes.js
@@ -1,19 +1,9 @@
 import React, { useRef } from "react";
 import "./Dishes.css";
 import { data } from "./DischesData";
-import { AiOutlineArrowRight } from "react-icons/ai";
-import { AiOutlineArrowLeft } from "react-icons/ai";
 
 export default function Dishes() {
   const ref = useRef();
-
-  const mealsHandleRight = () => {
-    ref.current.scrollLeft += 20;
-  };
-
-  const mealsHandleLeft = () => {
-    ref.current.scrollLeft -= 20;
-  };
 
   return (
     <div>
@@ -28,21 +18,6 @@ export default function Dishes() {
             </div>
           </div>
         ))}
-      </div>
-      <div
-        className="arrow"
-        style={{ bottom: "80px", right: "15px" }}
-        onClick={mealsHandleRight}
-      >
-        <AiOutlineArrowRight size={30} color="white" />
-      </div>
-
-      <div
-        className="arrow"
-        style={{ bottom: "80px", left: "15px" }}
-        onClick={mealsHandleLeft}
-      >
-        <AiOutlineArrowLeft size={30} color="white" />
       </div>
     </div>
   );
